64432092832cb04f1383978dbd551f712a762265
HER0MA <haoranma1994@gmail.com>
2018-09-15 21:28:09 -0700

    Num of Islands

---
+++ DFS&BFS/200. Number of Islands.java
@@ -0,0 +1,26 @@
+// t:m*n s:1
+class Solution {
+    public int numIslands(char[][] grid) {
+        if (grid==null || grid.length==0 || grid[0]==null || grid[0].length==0) return 0;
+        int m = grid.length;
+        int n = grid[0].length;
+        int count = 0;
+        for (int i=0; i<m; i++) {
+            for (int j=0; j<n; j++) {
+                if (grid[i][j]=='1') {
+                    count ++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return count;
+    }
+    private void dfs(char[][] grid, int i, int j) {
+        if (i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]!='1') return;
+        grid[i][j] = '2';
+        dfs(grid, i-1, j);
+        dfs(grid, i, j-1);
+        dfs(grid, i+1, j);
+        dfs(grid, i, j+1);
+    }
+}
