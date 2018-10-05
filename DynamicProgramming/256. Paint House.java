d04ad4e5a1713f96c07a372bea568ae7305f0023
HER0MA <haoranma1994@gmail.com>
2018-10-05 10:55:44 -0700

    Update Paint House

--- DynamicProgramming/256. Paint House.java
+++ DynamicProgramming/256. Paint House.java
@@ -1,3 +1,36 @@
+// t:n s:1
+class Solution {
+    public int minCost(int[][] costs) {
+        if (costs==null || costs.length==0) return 0;
+        int red = costs[0][0];
+        int blue = costs[0][1];
+        int green = costs[0][2];
+        for (int i=1; i<costs.length; i++) {
+            int prevRed = red;
+            int prevBlue = blue;
+            int prevGreen = green;
+            red = Math.min(prevBlue, prevGreen) + costs[i][0];
+            blue = Math.min(prevRed, prevGreen) + costs[i][1];
+            green = Math.min(prevRed, prevBlue) + costs[i][2];
+        }
+        return Math.min(Math.min(red, blue), green);
+    }
+}
+
+// t:n s:1
+public class PaintHouse {
+    public int minCost(int[][] costs) {
+        if (costs == null || costs[0].length == 0) return 0;
+        for (int i = 1; i < costs.length; i++) {
+            costs[i][0] += Math.min(costs[i - 1][1], costs[i - 1][2]);
+            costs[i][1] += Math.min(costs[i - 1][0], costs[i - 1][2]);
+            costs[i][2] += Math.min(costs[i - 1][0], costs[i - 1][1]);
+        }
+        int n = costs.length - 1;
+        return Math.min(Math.min(costs[n][0], costs[n][1]), costs[n][2]);
+    }
+}
+
 // t:n s:n
 class Solution {
     public int minCost(int[][] costs) {
@@ -15,19 +48,5 @@ class Solution {
             green[i] = Math.min(blue[i-1], red[i-1]) + costs[i][2];
         }
         return Math.min(Math.min(red[n-1], blue[n-1]), green[n-1]);
-    }
-}
-
-// t:n s:1
-public class PaintHouse {
-    public int minCost(int[][] costs) {
-        if (costs == null || costs[0].length == 0) return 0;
-        for (int i = 1; i < costs.length; i++) {
-            costs[i][0] += Math.min(costs[i - 1][1], costs[i - 1][2]);
-            costs[i][1] += Math.min(costs[i - 1][0], costs[i - 1][2]);
-            costs[i][2] += Math.min(costs[i - 1][0], costs[i - 1][1]);
-        }
-        int n = costs.length - 1;
-        return Math.min(Math.min(costs[n][0], costs[n][1]), costs[n][2]);
     }
 }
--- DynamicProgramming/265. Paint House II.java
+++ DynamicProgramming/265. Paint House II.java
@@ -1,8 +1,76 @@
-// t:n*k s:n*k
+// t:n*k s:k
 /**
  * min1 the index of minimum res
  * min2 the index of 2nd minimum res
  */
+class Solution {
+    public int minCostII(int[][] costs) {
+        if (costs==null || costs.length==0) return 0;
+        int n = costs.length;
+        int k = costs[0].length;
+        int[] state = new int[k];
+        int min = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+        for (int i=0; i<k; i++) {
+            state[i] = costs[0][i];
+            if (state[i]<=min) {
+                min2 = min;
+                min = state[i];
+            } else if (state[i]<min2) {
+                min2 = state[i];
+            }
+        }
+        for (int j=1; j<n; j++) {
+            int minPrev = min;
+            int min2Prev = min2;
+            min = Integer.MAX_VALUE;
+            min2 = Integer.MAX_VALUE;
+            for (int i=0; i<k; i++) {
+                state[i] = state[i]==minPrev ? min2Prev : minPrev;
+                state[i] += costs[j][i];
+                if (state[i]<=min) {
+                    min2 = min;
+                    min = state[i];
+                } else if (state[i]<min2) {
+                    min2 = state[i];
+                }
+            }
+        }
+        return min;
+    }
+}
+
+// t:n*k s:1
+public class PaintHouseII {
+    public int minCostII(int[][] costs) {
+        if (costs == null || costs.length == 0) return 0;
+        int n = costs.length;
+        int k = costs[0].length;
+
+        int min1 = -1, min2 = -1;
+        for (int i = 0; i < n; i++) {
+            int last1 = min1, last2 = min2;
+            min1 = -1; min2 = -1;
+            for (int j = 0; j < k; j++) {
+                if (j != last1) {
+                    costs[i][j] += last1 < 0 ? 0 : costs[i - 1][last1];
+                } else {
+                    costs[i][j] += last2 < 0 ? 0 : costs[i - 1][last2];
+                }
+
+                if (min1 < 0 || costs[i][j] < costs[i][min1]) {
+                    min2 = min1;
+                    min1 = j;
+                } else if (min2 < 0 || costs[i][j] < costs[i][min2]) {
+                    min2 = j;
+                }
+            }
+        }
+        return costs[n - 1][min1];
+    }
+}
+
+// t:n*k s:n*k
 class Solution {
     public int minCostII(int[][] costs) {
         if (costs==null || costs.length==0) return 0;
@@ -37,35 +105,5 @@ class Solution {
             }
         }
         return res[n-1][min1];
-    }
-}
-
-// t:n*k s:1
-public class PaintHouseII {
-    public int minCostII(int[][] costs) {
-        if (costs == null || costs.length == 0) return 0;
-        int n = costs.length;
-        int k = costs[0].length;
-
-        int min1 = -1, min2 = -1;
-        for (int i = 0; i < n; i++) {
-            int last1 = min1, last2 = min2;
-            min1 = -1; min2 = -1;
-            for (int j = 0; j < k; j++) {
-                if (j != last1) {
-                    costs[i][j] += last1 < 0 ? 0 : costs[i - 1][last1];
-                } else {
-                    costs[i][j] += last2 < 0 ? 0 : costs[i - 1][last2];
-                }
-
-                if (min1 < 0 || costs[i][j] < costs[i][min1]) {
-                    min2 = min1;
-                    min1 = j;
-                } else if (min2 < 0 || costs[i][j] < costs[i][min2]) {
-                    min2 = j;
-                }
-            }
-        }
-        return costs[n - 1][min1];
     }
 }
