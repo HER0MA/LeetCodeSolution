c45651634ed1b7313dec5f9d64be6845def808b7
HER0MA <haoranma1994@gmail.com>
2018-11-12 10:07:16 -0800

    modification

--- LinkedList/2. Add Two Numbers.java
+++ LinkedList/2. Add Two Numbers.java
@@ -9,26 +9,25 @@
 // t:n s:n
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode p1 = l1, p2 = l2;
         ListNode dummy = new ListNode(0);
         ListNode p = dummy;
-        int carry = 0;
         int sum = 0;
-        while (p1!=null || p2!=null) {
-            sum = carry;
-            if (p1!=null) {
-                sum += p1.val;
-                p1 = p1.next;
+        while (l1!=null || l2!=null) {
+            if (l1!=null) {
+                sum += l1.val;
+                l1 = l1.next;
             }
-            if (p2!=null) {
-                sum += p2.val;
-                p2 = p2.next;
+            if (l2!=null) {
+                sum += l2.val;
+                l2 = l2.next;
             }
             p.next = new ListNode(sum%10);
             p = p.next;
-            carry = sum/10;
+            sum /= 10;
         }
-        if (carry!=0) p.next = new ListNode(carry);
+        if (sum!=0) {
+            p.next = new ListNode(1);
+        }
         return dummy.next;
     }
 }
--- String/14. Longest Common Prefix.java
+++ String/14. Longest Common Prefix.java
@@ -1,13 +1,13 @@
-// t:n s:1
+// t:n*l^2 (n=strs.length l=longestStr.length) s:1
 class Solution {
     public String longestCommonPrefix(String[] strs) {
         if (strs==null || strs.length==0) return "";
-        String ans = strs[0];
+        String res = strs[0];
         for (int i=1; i<strs.length; i++) {
-            while (strs[i].indexOf(ans)!=0) {
-                ans = ans.substring(0, ans.length()-1);
+            while (strs[i].indexOf(res)!=0) { // naive string matching
+                res = res.substring(0, res.length()-1);
             }
         }
-        return ans;
+        return res;
     }
 }
